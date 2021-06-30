348f1632a63acc251c9a9c13824f9bcb2ac320cf
liamjung <liamjung@live.com>
2021-06-30 16:38:52 +0800

    feat: 触发器目录

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -16,69 +16,57 @@ module.exports = {
   themeConfig: {
     // 侧边导航
     sidebar: [{
-        text: '首页',
-        link: '/guide/index',
+      text: '首页',
+      link: '/guide/index',
+    }, {
+      text: '如何安装',
+      children: [{
+        text: 'Docker环境安装',
+        link: '/guide/installation-docker',
+      }, {
+        text: 'Liunx环境安装',
+        link: '/guide/installation-linux'
       },
-      {
-        text: '如何安装',
+      ]
+    }, {
+      text: '如何使用',
+      children: [{
+        text: '快速开始',
+        link: '/guide/quick-start',
+      }, {
+        text: '流程定义',
+        link: '/guide/flow-dsl'
+      }, {
+        text: '节点定义',
+        link: '/guide/node-dsl'
+      }, {
+        text: '密钥管理',
+        link: '/guide/secrets'
+      }, {
+        text: '触发器',
         children: [{
-            text: 'Docker环境安装',
-            link: '/guide/installation-docker',
-          },
-          {
-            text: 'Liunx环境安装',
-            link: '/guide/installation-linux'
-          },
-        ]
-      },
-      {
-        text: '如何使用',
-        children: [{
-            text: '快速开始',
-            link: '/guide/quick-start',
-          },
-          {
-            text: '流程定义',
-            link: '/guide/flow-dsl'
-          },
-          {
-            text: '节点定义',
-            link: '/guide/node-dsl'
-          },
-          {
-            text: '密钥管理',
-            link: '/guide/secrets'
-          },
-          {
-            text: 'Cron',
-            link: '/guide/cron'
-          },
-          {
-            text: 'Webhooks',
-            link: '/guide/webhooks'
-          },
-        ]
-      },
-      {
-        text: '高级主题',
-        children: [{
-            text: '变量',
-            link: '/guide/vars',
-          },
-          {
-            text: '网关',
-            link: '/guide/gateway'
-          },
-          {
-            text: '运维',
-            link: '/guide/operation'
-          },
-          {
-            text: '自定义节点',
-            link: '/guide/custom-node'
-          },
-        ]
-      },
-    ]
+          text: 'Cron',
+          link: '/guide/cron',
+        }],
+      }, {
+        text: 'Webhooks',
+        link: '/guide/webhooks'
+      }],
+    }, {
+      text: '高级主题',
+      children: [{
+        text: '变量',
+        link: '/guide/vars',
+      }, {
+        text: '网关',
+        link: '/guide/gateway'
+      }, {
+        text: '运维',
+        link: '/guide/operation'
+      }, {
+        text: '自定义节点',
+        link: '/guide/custom-node'
+      }],
+    }],
   }
 }
