e2142cd75f34463c55c58105f5b4e37b6fb2b838
liamjung <liamjung@live.com>
2021-10-19 14:35:30 +0800

    feat: statistics

---
+++ .vitepress/baidu-tongji.js
@@ -0,0 +1,8 @@
+console.debug('Baidu tongji installed');
+const _hmt = [];
+(function () {
+  const hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?beb9a87efce5424df2ecbbd3caa68d81';
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,6 @@
 import DefaultTheme from 'vitepress/dist/client/theme-default'
 import './custom.css'
+// 安装百度统计
+import '../baidu-tongji'
 
 export default DefaultTheme
