43e7749568387f506899b86559a1fe66f5269c40
liamjung <liamjung@live.com>
2021-10-19 15:37:45 +0800

    fix: statistics config

--- .vitepress/baidu-tongji.js
+++
@@ -1,8 +0,0 @@
-console.debug('Baidu tongji installed');
-const _hmt = [];
-(function () {
-  const hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?beb9a87efce5424df2ecbbd3caa68d81';
-  const s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-})();
--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,95 +1,102 @@
 module.exports = {
-    title: '持续集成平台',
-    description: 'jianmu持续集成平台文档',
-    lang: 'zh-CN',
-    head: [
-        // 改变title的图标
-        [
-            'link',
-            {
-                rel: 'icon',
-                href: '/favicon.ico',
-            },
-        ],
+  title: '持续集成平台',
+  description: 'jianmu持续集成平台文档',
+  lang: 'zh-CN',
+  head: [
+    // 改变title的图标
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
     ],
-    // 主题配置
-    themeConfig: {
-        // 侧边导航
-        sidebar: [{
-            text: '首页',
-            link: '/guide/index',
+    // 百度统计
+    [
+      'script',
+      {
+        src: "https://hm.baidu.com/hm.js?beb9a87efce5424df2ecbbd3caa68d81",
+      }
+    ],
+  ],
+  // 主题配置
+  themeConfig: {
+    // 侧边导航
+    sidebar: [{
+      text: '首页',
+      link: '/guide/index',
+    }, {
+      text: '如何安装',
+      children: [{
+        text: 'Docker环境安装',
+        link: '/guide/installation-docker',
+      }, {
+        text: 'Linux环境安装',
+        link: '/guide/installation-linux'
+      },]
+    }, {
+      text: '如何使用',
+      children: [{
+        text: '快速开始',
+        link: '/guide/quick-start',
+      }, {
+        text: '触发器',
+        children: [{
+          text: '事件桥接器',
+          link: '/guide/event-bridge'
         }, {
-            text: '如何安装',
-            children: [{
-                text: 'Docker环境安装',
-                link: '/guide/installation-docker',
-            }, {
-                text: 'Linux环境安装',
-                link: '/guide/installation-linux'
-            },]
-        }, {
-            text: '如何使用',
-            children: [{
-                text: '快速开始',
-                link: '/guide/quick-start',
-            }, {
-                text: '触发器',
-                children: [{
-                    text: '事件桥接器',
-                    link: '/guide/event-bridge'
-                }, {
-                    text: 'Cron',
-                    link: '/guide/cron',
-                }],
-                // }, {
-                //     text: 'Webhooks',
-                //     link: '/guide/webhooks'
-            }, {
-                text: '流程编排',
-                children: [{
-                    text: '流程定义',
-                    link: '/guide/flow-dsl'
-                }, {
-                    text: '管道定义',
-                    link: '/guide/pipeline-dsl'
-                }],
-            }, {
-                text: '节点定义',
-                link: '/guide/node-dsl'
-            }, {
-                text: '密钥管理',
-                link: '/guide/secrets'
-            }],
-        }, {
-            text: '高级主题',
-            children: [{
-                text: '参数',
-                link: '/guide/vars',
-            }, {
-                text: "表达式",
-                link: "/guide/expression"
-            }, {
-                text: '内置节点',
-                children: [{
-                    text: "开始/结束节点",
-                    link: "/guide/start-end-node"
-                }, {
-                    text: "网关节点",
-                    link: "/guide/gateway"
-                }]
-            },
-                // {
-                //     text: '运维',
-                //     link: '/guide/operation'
-                // },
-                {
-                    text: '自定义节点',
-                    link: '/guide/custom-node'
-                }, {
-                    text: '第三方库',
-                    link: '/guide/license-mixing'
-                }
-            ],
+          text: 'Cron',
+          link: '/guide/cron',
         }],
-    }
+        // }, {
+        //     text: 'Webhooks',
+        //     link: '/guide/webhooks'
+      }, {
+        text: '流程编排',
+        children: [{
+          text: '流程定义',
+          link: '/guide/flow-dsl'
+        }, {
+          text: '管道定义',
+          link: '/guide/pipeline-dsl'
+        }],
+      }, {
+        text: '节点定义',
+        link: '/guide/node-dsl'
+      }, {
+        text: '密钥管理',
+        link: '/guide/secrets'
+      }],
+    }, {
+      text: '高级主题',
+      children: [{
+        text: '参数',
+        link: '/guide/vars',
+      }, {
+        text: "表达式",
+        link: "/guide/expression"
+      }, {
+        text: '内置节点',
+        children: [{
+          text: "开始/结束节点",
+          link: "/guide/start-end-node"
+        }, {
+          text: "网关节点",
+          link: "/guide/gateway"
+        }]
+      },
+        // {
+        //     text: '运维',
+        //     link: '/guide/operation'
+        // },
+        {
+          text: '自定义节点',
+          link: '/guide/custom-node'
+        }, {
+          text: '第三方库',
+          link: '/guide/license-mixing'
+        }
+      ],
+    }],
+  }
 }
