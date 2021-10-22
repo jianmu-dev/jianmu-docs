2eb22159aaf49289c6b800db9a41328650f8af38
liamjung <liamjung@live.com>
2021-10-22 11:52:11 +0800

    fix: home link

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,18 @@
 import DefaultTheme from 'vitepress/dist/client/theme-default'
 import './custom.css'
 
-export default DefaultTheme
+const HOME_PAGE = '/guide/'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({router}) {
+    // 保证页面渲染完成
+    setTimeout(() => {
+      if (router.route.path === '/') {
+        router.go(HOME_PAGE)
+      }
+
+      document.getElementsByClassName('nav-bar-title')[0].href = HOME_PAGE
+    }, 200);
+  }
+}
